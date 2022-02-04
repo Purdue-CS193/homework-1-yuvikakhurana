f15e0438b35bb7c334fb8546750478065d670745
Yuvika Khurana <ykhuran@data.cs.purdue.edu>
2022-02-04 01:38:14 -0500

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,48 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
+mv Memes/'Star Wars'/fafsa.jpg Memes/SpongeBob/fafsa.jpg
 
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++
 
 # Task 7
-
+cp -r Programs/Java Homework/CS180/Project_1 
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py Programs/'Python 3'
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls /homes/ykhuran/homework-1-yuvikakhurana 
 
 # Task 12
-: '
+: ' 
 Type your vim/nano customizations below:
-
+set number
+set laststatus=2
+colorscheme darkblue
+set wrap
 
 
 '
+
+
